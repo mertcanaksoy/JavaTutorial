71cdfcea8ab8f0cbb645bb7d50a7497ebda432f4
Mert Aksoy <36101620+mertcanaksoy@users.noreply.github.com>
2019-07-17 14:00:15 +0300

    Add files via upload

---
+++ 2)Methods/5)getName().java
@@ -0,0 +1,23 @@
+package com.mert.javatutorial;
+public class GFG 
+{ 
+    public static void main(String arr[]) 
+    { 
+        Object y; 
+  
+        y = 'A'; 
+        System.out.println(y.getClass().getName()); 
+  
+        y = 1; 
+        System.out.println(y.getClass().getName()); 
+  
+        y = "Hi"; 
+        System.out.println(y.getClass().getName()); 
+  
+        y = 1.222; 
+        System.out.println(y.getClass().getName()); 
+  
+        y = false; 
+        System.out.println(y.getClass().getName()); 
+    } 
+} 
